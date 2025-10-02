180e512f4c810b8f5504952db760b470cc01ff8f
Nayan17107 <prajapatinayan17107@gmail.com>
2025-10-02 10:13:42 +0530

    02/10/2025

---
+++ PR-3/src/App.jsx
@@ -0,0 +1,32 @@
+import './index.css';
+import Header from './Components/Header';
+import Banner from './Components/Banner';
+import Causes from './Components/Causes';
+import Help from './Components/Help';
+import Gallary from './Components/Gallary';
+import Events from './Components/Events';
+import Team from './Components/Team';
+import Review from './Components/Review';
+import Blog from './Components/Blog';
+import Sponcer from './Components/Sponcer';
+import Footer from './Components/Footer';
+
+function App() {
+    return (
+        <>
+        <Header />,
+        <Banner />,
+        <Causes />,
+        <Help />,
+        <Gallary />,
+        <Events />,
+        <Team />,
+        <Review />,
+        <Blog />,
+        <Sponcer />,
+        <Footer />
+        </>
+    )
+}
+
+export default App;
--- PR-3/src/main.jsx
+++ PR-3/src/main.jsx
@@ -1,30 +1,9 @@
-import './index.css';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
-import Header from './Components/Header';
-import Banner from './Components/Banner';
-import Causes from './Components/Causes';
-import Help from './Components/Help';
-import Gallary from './Components/Gallary';
-import Events from './Components/Events';
-import Team from './Components/Team';
-import Review from './Components/Review';
-import Blog from './Components/Blog';
-import Sponcer from './Components/Sponcer';
-import Footer from './Components/Footer';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-    <Header/>,
-    <Banner/>,
-    <Causes/>,
-    <Help/>,
-    <Gallary/>,
-    <Events/>,
-    <Team/>,
-    <Review/>,
-    <Blog/>,
-    <Sponcer/>,
-    <Footer/>
+        <App/>
     </StrictMode>
 )
